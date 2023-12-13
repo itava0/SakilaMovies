bb2b22fe5ae45c08bc6a44084ff90ab7b1b70ace
Ivan Tavarez <ivantavarez2@gmail.com>
2023-12-13 13:44:31 -0500

    Added comments

--- src/main/java/org/pluralsight/App.java
+++ src/main/java/org/pluralsight/App.java
@@ -4,25 +4,35 @@ import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.SQLException;
 import java.util.*;
+
+/**
+ * The main application class for interacting with the Sakila Movies database.
+ */
 public class App {
     public static Scanner scan = new Scanner(System.in);
     public static String userInput = "";
 
     public static void main(String[] args) {
-
+        // Set up the DataSource for database connection
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setUrl("jdbc:mysql://localhost:3306/Sakila");
         dataSource.setUsername(System.getenv("MY_DB_USERNAME"));
         dataSource.setPassword(System.getenv("MY_DB_PASSWORD"));
         SakilaDataManager dataManager = new SakilaDataManager(dataSource);
+
         System.out.println("Welcome to Sakila Movies!");
+
+        // Main application loop
         while (true) {
+            // Display main menu options
             System.out.println("""
                     What would you like to do?
                     1) Search by Actor
                     2) Search by Film
                     0) Exit
                     Please enter your choice as the corresponding number\t""");
+
+            // Get user input and perform corresponding action
             userInput = scan.nextLine();
             switch (userInput) {
                 case "1":
@@ -47,17 +57,26 @@ public class App {
         }
     }
 
+    /**
+     * Method to handle actor search options.
+     *
+     * @param dataManager The SakilaDataManager for database operations.
+     */
     public static void searchByActor(SakilaDataManager dataManager) {
         do {
+            // Display actor search menu options
             System.out.println("""
                     How would you like to search?
                     1) Actor's Last Name
                     2) Actor's First Name
                     0) Return to Home
                     Please enter your choice as the corresponding number\t""");
+
+            // Get user input and perform corresponding action
             userInput = scan.nextLine();
             switch (userInput) {
                 case "1":
+                    // Search actors by last name
                     System.out.print("Enter the Last Name of the actor:");
                     userInput = scan.nextLine();
                     List<Actor> actors = dataManager.getActorByLast(userInput);
@@ -68,6 +87,7 @@ public class App {
                     }
                     break;
                 case "2":
+                    // Search actors by first name
                     System.out.print("Enter the First Name of the actor:");
                     userInput = scan.nextLine();
                     List<Actor> actors1 = dataManager.getActorByFirst(userInput);
@@ -86,17 +106,27 @@ public class App {
             }
         } while (!userInput.equalsIgnoreCase("0"));
     }
+
+    /**
+     * Method to handle film search options.
+     *
+     * @param dataManager The SakilaDataManager for database operations.
+     */
     public static void searchFilmByActor(SakilaDataManager dataManager) {
         do {
-        System.out.println("""
-                How would you like to search?
-                1) Actor's Full Name
-                2) Actor's ID
-                0) Return to Home
-                Please enter your choice as the corresponding number""");
-        userInput = scan.nextLine();
+            // Display film search menu options
+            System.out.println("""
+                    How would you like to search?
+                    1) Actor's Full Name
+                    2) Actor's ID
+                    0) Return to Home
+                    Please enter your choice as the corresponding number""");
+
+            // Get user input and perform corresponding action
+            userInput = scan.nextLine();
             switch (userInput) {
                 case "1":
+                    // Search films by actor's full name
                     System.out.print("Enter the First Name and Last Name of the actor: ");
                     userInput = scan.nextLine();
                     List<Film> filmList = dataManager.getMovieFromActor(userInput);
@@ -107,6 +137,7 @@ public class App {
                     }
                     break;
                 case "2":
+                    // Search films by actor's ID
                     System.out.print("Enter the ID of the actor: ");
                     userInput = scan.nextLine();
                     List<Film> filmList1 = dataManager.getMovieFromActorID(userInput);
--- src/main/java/org/pluralsight/SakilaDataManager.java
+++ src/main/java/org/pluralsight/SakilaDataManager.java
@@ -3,25 +3,41 @@ import javax.sql.DataSource;
 import java.sql.*;
 import java.util.*;
 
+/**
+ * This class provides data access methods for interacting with the Sakila database.
+ */
 public class SakilaDataManager {
     private DataSource dataSource;
+
+    /**
+     * Constructs a SakilaDataManager with the specified DataSource.
+     *
+     * @param dataSource The DataSource to be used for database connections.
+     */
     public SakilaDataManager(DataSource dataSource){
         this.dataSource = dataSource;
     }
+
+    /**
+     * Retrieves a list of actors based on the last name.
+     *
+     * @param name The last name of the actors to search for.
+     * @return A list of Actor objects matching the last name, or null if an error occurs.
+     */
     public List<Actor> getActorByLast(String name){
         try(Connection connection = dataSource.getConnection(); PreparedStatement statement = connection.prepareStatement("select * from actor where last_name = ?")){
             statement.setString(1, name);
-            List<Actor> a = new ArrayList<>();
+            List<Actor> actors = new ArrayList<>();
             try(ResultSet results = statement.executeQuery()){
                 while(results.next()){
-                    a.add(new Actor(results.getInt(1), results.getString(2), results.getString(3)));
+                    actors.add(new Actor(results.getInt(1), results.getString(2), results.getString(3)));
                 }
             }
             catch(SQLException e){
                 e.printStackTrace();
                 System.out.println("Error occurred during the search");
             }
-            return a;
+            return actors;
         }
         catch(SQLException e){
             e.printStackTrace();
@@ -29,20 +45,27 @@ public class SakilaDataManager {
         }
         return null;
     }
+
+    /**
+     * Retrieves a list of actors based on the first name.
+     *
+     * @param name The first name of the actors to search for.
+     * @return A list of Actor objects matching the first name, or null if an error occurs.
+     */
     public List<Actor> getActorByFirst(String name){
         try(Connection connection = dataSource.getConnection(); PreparedStatement statement = connection.prepareStatement("select * from actor where first_name = ?")){
             statement.setString(1, name);
-            List<Actor> a = new ArrayList<>();
+            List<Actor> actors = new ArrayList<>();
             try(ResultSet results = statement.executeQuery()){
                 while(results.next()){
-                    a.add(new Actor(results.getInt(1), results.getString(2), results.getString(3)));
+                    actors.add(new Actor(results.getInt(1), results.getString(2), results.getString(3)));
                 }
             }
             catch(SQLException e){
                 e.printStackTrace();
                 System.out.println("Error occurred during the search");
             }
-            return a;
+            return actors;
         }
         catch(SQLException e){
             e.printStackTrace();
@@ -50,6 +73,13 @@ public class SakilaDataManager {
         }
         return null;
     }
+
+    /**
+     * Retrieves a list of movies in which an actor has participated based on the full name.
+     *
+     * @param name The full name of the actor.
+     * @return A list of Film objects, or null if an error occurs.
+     */
     public List<Film> getMovieFromActor(String name){
         String[] names = name.split(" ");
         try(Connection connection = dataSource.getConnection(); PreparedStatement statement = connection.prepareStatement("select film.* from film " +
@@ -58,38 +88,45 @@ public class SakilaDataManager {
                 "where first_name = ? AND last_name = ?")){
             statement.setString(1, names[0]);
             statement.setString(2, names[1]);
-            List<Film> m = new ArrayList<>();
+            List<Film> movies = new ArrayList<>();
             try(ResultSet results = statement.executeQuery()){
                 while(results.next()){
-                    m.add(new Film(results.getInt(1), results.getInt(4), results.getInt(9), results.getString(2), results.getString(3)));
+                    movies.add(new Film(results.getInt(1), results.getInt(4), results.getInt(9), results.getString(2), results.getString(3)));
                 }
             }
             catch(SQLException e){
                 System.out.println("Error occurred during the search");
             }
-            return m;
+            return movies;
         }
         catch(SQLException e){
             System.out.println("Error occurred during the search");
         }
         return null;
     }
+
+    /**
+     * Retrieves a list of movies in which an actor has participated based on the actor's ID.
+     *
+     * @param id The ID of the actor.
+     * @return A list of Film objects, or null if an error occurs.
+     */
     public List<Film> getMovieFromActorID(String id){
         try(Connection connection = dataSource.getConnection(); PreparedStatement statement = connection.prepareStatement("select film.* from film " +
                 "join film_actor on film.film_id = film_actor.film_id " +
                 "join actor on film_actor.actor_id = actor.actor_id " +
                 "where actor.actor_id = ?")){
             statement.setInt(1, Integer.parseInt(id));
-            List<Film> m = new ArrayList<>();
+            List<Film> movies = new ArrayList<>();
             try(ResultSet results = statement.executeQuery()){
                 while(results.next()){
-                    m.add(new Film(results.getInt(1), results.getInt(4), results.getInt(9), results.getString(2), results.getString(3)));
+                    movies.add(new Film(results.getInt(1), results.getInt(4), results.getInt(9), results.getString(2), results.getString(3)));
                 }
             }
             catch(SQLException e){
                 System.out.println("Error occurred during the search");
             }
-            return m;
+            return movies;
         }
         catch(SQLException e){
             System.out.println("Error occurred during the search");
